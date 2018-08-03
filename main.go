794f215afce52b797e444ed9eedcb493b860ee78
Daniel Nordstrom <anitox@gmail.com>
2018-08-03 10:39:47 -0400

    Moved functions to separate file

--- main.go
+++ main.go
@@ -1,29 +1,3 @@
 package main
 
-import "fmt"
-
-func main() {
-	x := 5
-	y := 4
-
-	fmt.Printf("%d + %d = %d\n", x, y, Add(x, y))
-	fmt.Printf("%d - %d = %d\n", x, y, Sub(x, y))
-}
-
-// Add sums one or more values
-func Add(nums ...int) (sum int) {
-	for _, num := range nums {
-		sum += num
-	}
-	return
-}
-
-// Sub subtracts numbers from the first number
-func Sub(nums ...int) (total int) {
-	total = nums[0]
-
-	for _, num := range nums[1:] {
-		total -= num
-	}
-	return
-}
+func main() {}
